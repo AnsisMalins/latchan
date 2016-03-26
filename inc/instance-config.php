40465fd6139fe46264af845ba710064515ab6c7a
Ansis Malins <ansis.malins@gmail.com>
2016-03-26 10:20:31 +0100

    Latchan specific configuration.

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -1,23 +1,25 @@
 <?php
 
 /*
- *  Instance Configuration
- *  ----------------------
- *  Edit this file and not config.php for imageboard configuration.
- *
- *  You can copy values from config.php (defaults) and paste them here.
- */
+*  Instance Configuration
+*  ----------------------
+*  Edit this file and not config.php for imageboard configuration.
+*
+*  You can copy values from config.php (defaults) and paste them here.
+*/
 
+require('secret-config.php');
 
+$config['root'] = '/';
 
-	// Database stuff
-	$config['db']['type']		= 'mysql';
-	$config['db']['server']		= 'localhost';
-	$config['db']['user']		= '';
-	$config['db']['password']	= '';
-	$config['db']['database']	= '';
-	
-	//$config['root']				= '/';
-	
-	
-?>
+$config['thumb_width'] = 128;
+$config['thumb_height'] = 128;
+$config['thumb_op_width'] = 256;
+$config['thumb_op_height'] = 256;
+
+$config['allowed_ext_files'][] = 'webm';
+$config['additional_javascript'][] = 'js/jquery.min.js';
+$config['additional_javascript'][] = 'js/catalog-link.js';
+$config['additional_javascript'][] = 'js/expand-video.js';
+$config['additional_javascript'][] = 'js/show-op.js';
+$config['additional_javascript'][] = 'js/webm-settings.js';
