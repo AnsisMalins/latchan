c6b0d2d5c245a42eadd103d3d7b2c8166fc48b65
Ansis Malins <ansis.malins@gmail.com>
2016-03-26 10:19:14 +0100

    Made webms behave exactly like gifs.

--- js/expand-video.js
+++ js/expand-video.js
@@ -10,13 +10,8 @@ function setupVideo(thumb, url) {
     thumb.videoAlreadySetUp = true;
 
     var video = null;
-    var videoContainer, videoHide;
+    var videoContainer;
     var expanded = false;
-    var hovering = false;
-    var loop = true;
-    var loopControls = [document.createElement("span"), document.createElement("span")];
-    var fileInfo = thumb.parentNode.querySelector(".fileinfo");
-    var mouseDown = false;
 
     function unexpand() {
         if (expanded) {
@@ -29,55 +24,21 @@ function setupVideo(thumb, url) {
         }
     }
 
-    function unhover() {
-        if (hovering) {
-            hovering = false;
-            if (video.pause) video.pause();
-            videoContainer.style.display = "none";
-            video.style.maxWidth = "inherit";
-            video.style.maxHeight = "inherit";
-        }
-    }
-
     // Create video element if does not exist yet
     function getVideo() {
         if (video == null) {
             video = document.createElement("video");
             video.src = url;
-            video.loop = loop;
+            video.loop = true;//loop;
             video.innerText = _("Your browser does not support HTML5 video.");
-
-            videoHide = document.createElement("img");
-            videoHide.src = configRoot + "static/collapse.gif";
-            videoHide.alt = "[ - ]";
-            videoHide.title = "Collapse video";
-            videoHide.style.marginLeft = "-15px";
-            videoHide.style.cssFloat = "left";
-            videoHide.addEventListener("click", unexpand, false);
+            video.addEventListener("click", unexpand, false);
+            video.style.cursor = "pointer";
 
             videoContainer = document.createElement("div");
             videoContainer.style.paddingLeft = "15px";
             videoContainer.style.display = "none";
-            videoContainer.appendChild(videoHide);
             videoContainer.appendChild(video);
             thumb.parentNode.insertBefore(videoContainer, thumb.nextSibling);
-
-            // Dragging to the left collapses the video
-            video.addEventListener("mousedown", function(e) {
-                if (e.button == 0) mouseDown = true;
-            }, false);
-            video.addEventListener("mouseup", function(e) {
-                if (e.button == 0) mouseDown = false;
-            }, false);
-            video.addEventListener("mouseenter", function(e) {
-                mouseDown = false;
-            }, false);
-            video.addEventListener("mouseout", function(e) {
-                if (mouseDown && e.clientX - video.getBoundingClientRect().left <= 0) {
-                    unexpand();
-                }
-                mouseDown = false;
-            }, false);
         }
     }
 
@@ -86,20 +47,16 @@ function setupVideo(thumb, url) {
         if (setting("videoexpand") && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) {
             getVideo();
             expanded = true;
-            hovering = false;
 
             video.style.position = "static";
             video.style.pointerEvents = "inherit";
             video.style.display = "inline";
-            videoHide.style.display = "inline";
             videoContainer.style.display = "block";
             videoContainer.style.position = "static";
             video.parentNode.parentNode.removeAttribute('style');
             thumb.style.display = "none";
 
-            video.muted = (setting("videovolume") == 0);
-            video.volume = setting("videovolume");
-            video.controls = true;
+            video.muted = true;
             if (video.readyState == 0) {
                 video.addEventListener("loadedmetadata", expand2, false);
             } else {
@@ -117,87 +74,6 @@ function setupVideo(thumb, url) {
         if (bottom > window.innerHeight) {
             window.scrollBy(0, bottom - window.innerHeight);
         }
-        // work around Firefox volume control bug
-        video.volume = Math.max(setting("videovolume") - 0.001, 0);
-        video.volume = setting("videovolume");
-    }
-
-    // Hovering over thumbnail displays video
-    thumb.addEventListener("mouseover", function(e) {
-        if (setting("videohover")) {
-            getVideo();
-            expanded = false;
-            hovering = true;
-
-            var docRight = document.documentElement.getBoundingClientRect().right;
-            var thumbRight = thumb.querySelector("img, video").getBoundingClientRect().right;
-            var maxWidth = docRight - thumbRight - 20;
-            if (maxWidth < 250) maxWidth = 250;
-
-            video.style.position = "fixed";
-            video.style.right = "0px";
-            video.style.top = "0px";
-            var docRight = document.documentElement.getBoundingClientRect().right;
-            var thumbRight = thumb.querySelector("img, video").getBoundingClientRect().right;
-            video.style.maxWidth = maxWidth + "px";
-            video.style.maxHeight = "100%";
-            video.style.pointerEvents = "none";
-
-            video.style.display = "inline";
-            videoHide.style.display = "none";
-            videoContainer.style.display = "inline";
-            videoContainer.style.position = "fixed";
-
-            video.muted = (setting("videovolume") == 0);
-            video.volume = setting("videovolume");
-            video.controls = false;
-            video.play();
-        }
-    }, false);
-
-    thumb.addEventListener("mouseout", unhover, false);
-
-    // Scroll wheel on thumbnail adjusts default volume
-    thumb.addEventListener("wheel", function(e) {
-        if (setting("videohover")) {
-            var volume = setting("videovolume");
-            if (e.deltaY > 0) volume -= 0.1;
-            if (e.deltaY < 0) volume += 0.1;
-            if (volume < 0) volume = 0;
-            if (volume > 1) volume = 1;
-            if (video != null) {
-                video.muted = (volume == 0);
-                video.volume = volume;
-            }
-            changeSetting("videovolume", volume);
-            e.preventDefault();
-        }
-    }, false);
-
-    // [play once] vs [loop] controls
-    function setupLoopControl(i) {
-        loopControls[i].addEventListener("click", function(e) {
-            loop = (i != 0);
-            thumb.href = thumb.href.replace(/([\?&])loop=\d+/, "$1loop=" + i);
-            if (video != null) {
-                video.loop = loop;
-                if (loop && video.currentTime >= video.duration) {
-                    video.currentTime = 0;
-                }
-            }
-            loopControls[i].style.fontWeight = "bold";
-            loopControls[1-i].style.fontWeight = "inherit";
-        }, false);
-    }
-
-    loopControls[0].textContent = _("[play once]");
-    loopControls[1].textContent = _("[loop]");
-    loopControls[1].style.fontWeight = "bold";
-    for (var i = 0; i < 2; i++) {
-        setupLoopControl(i);
-        loopControls[i].style.whiteSpace = "nowrap";
-        fileInfo.appendChild(document.createTextNode(" "));
-        fileInfo.appendChild(loopControls[i]);
     }
 }
 
@@ -217,10 +93,6 @@ function setupVideosIn(element) {
 }
 
 onready(function(){
-    // Insert menu from settings.js
-    if (typeof settingsMenu != "undefined" && typeof Options == "undefined")
-      document.body.insertBefore(settingsMenu, document.getElementsByTagName("hr")[0]);
-
     // Setup Javascript events for videos in document now
     setupVideosIn(document);
 
